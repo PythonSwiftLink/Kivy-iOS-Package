572aa6d4aa01cdc320399aadcc777d62cc60f883
PythonSwiftLink <107263226+PythonSwiftLink@users.noreply.github.com>
2023-10-03 22:44:25 +0200

    ..

--- create_package.sh
+++ create_package.sh
@@ -5,4 +5,4 @@ git clone https://github.com/PythonSwiftLink/PythonSwiftLink
 cp -rf xcframework PythonSwiftLink/Sources/PythonLib/
 cp -f Package.swift PythonSwiftLink
 cp -rf KivyLauncher PythonSwiftLink/Sources/KivyLauncher
-mv PythonSwiftLink release
+mv PythonSwiftLink Kivy-iOS-Package
